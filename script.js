06e5b388c376f8e6772eb60a9ea94771401a9346
David <sambunjak.david@gmail.com>
2025-09-01 00:52:26 +0200

    Add calculator functionality
    A basic functionality flow has been added to the calculator. All buttons
    apart from float button are working. Next, I have to make sure that the
    calculator has only 2 operands at the time so the user could chain
    operations without buggy behavior. Bug that I've also noticed is too
    many decimal numbers sending the screen into overflow. Needs fixing.

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-let firstOperand;
-let secondOperand;
-const operators = ["addition", "subtraction", "multiplication", "division"];
+let firstOperand = 0;
+let secondOperand = 0;
+let currentOperator = "+";
+let isOperatorOnScreen = false;
+
 const operations = {
   add(a, b) {
     return a + b;
@@ -19,16 +21,68 @@ const operations = {
 function operate(a, b, operator) {
   switch (operator) {
     case "addition":
-      operations.add(a, b);
-      break;
+      return operations.add(a, b);
     case "subtraction":
-      operations.subtract(a, b);
-      break;
+      return operations.subtract(a, b);
     case "multiplication":
-      operations.multiply(a, b);
-      break;
+      return operations.multiply(a, b);
     case "division":
-      operations.divide(a, b);
-      break;
+      return operations.divide(a, b);
   }
 }
+
+let screen = document.querySelector("#calculator-screen");
+screen.textContent = "0";
+
+// Operator buttons
+let operatorButtons = document.querySelectorAll(".operator-button");
+operatorButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    firstOperand = parseInt(screen.textContent);
+    screen.textContent = button.textContent;
+    isOperatorOnScreen = true;
+
+    function getCurrentOperator() {
+      switch (screen.textContent) {
+        case "+":
+          return "addition";
+        case "-":
+          return "subtraction";
+        case "*":
+          return "multiplication";
+        case "/":
+          return "division";
+        default:
+          return "";
+      }
+    }
+
+    currentOperator = getCurrentOperator();
+  });
+});
+
+// Digit buttons
+let digitButtons = document.querySelectorAll(".digit-num");
+digitButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    if (isOperatorOnScreen) {
+      screen.textContent = parseInt(button.textContent);
+      isOperatorOnScreen = false;
+    } else {
+      screen.textContent = parseInt(screen.textContent + button.textContent);
+    }
+  });
+});
+
+// Execute operation (=)
+let execute = document.querySelector("#execute");
+execute.addEventListener("click", () => {
+  secondOperand = parseInt(screen.textContent);
+  screen.textContent = operate(firstOperand, secondOperand, currentOperator);
+});
+
+// Clear
+let clearButton = document.querySelector("#clear-button");
+clearButton.addEventListener("click", () => {
+  screen.textContent = "0";
+});
